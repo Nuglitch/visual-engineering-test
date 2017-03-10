c89b9a0684034fba8aa0d3e5435c19acc0b7aae0
Oscar <oscar.mp10@gmail.com>
2017-03-10 02:53:58 +0100

    Delete element animation first version

--- public/js/modules/shop-cart.js
+++ public/js/modules/shop-cart.js
@@ -11,7 +11,22 @@ define(['jquery'], function($) {
      * private method to displau UI and bind user action events
      */
     displayUI: function() {
+      $('.delete').on('click', function(e) {
+        _private.moveRight(this.closest('.shop-cart-item'));
+      });
+    },
 
+    moveRight: function(elem) {
+      var leftPosition = parseInt($(elem).css('left')) + 20;
+      $(elem).css('left', leftPosition + 'px');
+      
+      var animate
+      if (leftPosition < window.innerWidth) {
+        animate = setTimeout(_private.moveRight.bind(null, elem), 10); // call moveRight in 20msec
+      } else {
+        $(elem).remove();
+        clearTimeout(animate);
+      }
     }
   };
 
