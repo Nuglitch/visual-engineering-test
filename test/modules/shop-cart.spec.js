19b995ea67c25de57fc0e351036bb7d7126da8ec
Oscar <oscar.mp10@gmail.com>
2017-03-12 20:02:03 +0100

    Shop cart test added

--- test/modules/shop-cart.spec.js
+++ test/modules/shop-cart.spec.js
@@ -9,26 +9,28 @@ define(['modules/shop-cart'], function(ShopCart) {
       expect(ShopCart.init).toBeDefined();
     });
 
-    it('buy button should be disabled if there are no items in the cart', function() {
+    describe('buy button should be disabled if there are no items in the cart', function() {
       var list = document.createElement("div");
 
       var button = document.createElement("button");
 
-      //check empty list
-      ShopCart.apiTest.checkIfBuyButtonIsEnable(list, button);
-      expect(button.getAttribute("disabled")).toBeDefined();
-      expect(button.getAttribute("disabled")).toMatch("disabled");
+      it('check empty list', function() {
+        ShopCart.apiTest.checkIfBuyButtonIsEnable(list, button);
+        expect(button.getAttribute("disabled")).toBeDefined();
+        expect(button.getAttribute("disabled")).toMatch("disabled");
+      });
+      
+      it('check list with one item', function() {
+        var item = document.createElement("article");
+        item.classList.add("shop-cart-item");
+        list.appendChild(item);
 
-      var item = document.createElement("article");
-      item.classList.add("shop-cart-item");
-      list.appendChild(item);
-
-      //check list with one item
-      ShopCart.apiTest.checkIfBuyButtonIsEnable(list, button);
-      expect(button.getAttribute("disabled")).toBe(null);
+        ShopCart.apiTest.checkIfBuyButtonIsEnable(list, button);
+        expect(button.getAttribute("disabled")).toBe(null);
+      });
     });
 
-    it('buy button should be disabled if there is one or more items out of stock', function() {
+    describe('buy button should be disabled if there is one or more items out of stock', function() {
       var list = document.createElement("div");
 
       var item = document.createElement("article");
@@ -41,28 +43,31 @@ define(['modules/shop-cart'], function(ShopCart) {
 
       var button = document.createElement("button");
 
-      //check list without elemts out of stock
-      ShopCart.apiTest.checkIfBuyButtonIsEnable(list, button);
-      expect(button.getAttribute("disabled")).toBe(null);
+      it('check list without elemts out of stock', function() {
+        ShopCart.apiTest.checkIfBuyButtonIsEnable(list, button);
+        expect(button.getAttribute("disabled")).toBe(null);
+      });
 
-      var itemOutStockA = document.createElement("article");
-      itemOutStockA.classList.add("out-of-stock"); 
-      list.appendChild(itemOutStockA); 
+      it('check list with one element out of stock', function() {
+        var itemOutStockA = document.createElement("article");
+        itemOutStockA.classList.add("out-of-stock"); 
+        list.appendChild(itemOutStockA); 
 
-      //check list with one element out of stock
-      ShopCart.apiTest.checkIfBuyButtonIsEnable(list, button);
-      expect(button.getAttribute("disabled")).toBeDefined();
-      expect(button.getAttribute("disabled")).toMatch("disabled");
+        ShopCart.apiTest.checkIfBuyButtonIsEnable(list, button);
+        expect(button.getAttribute("disabled")).toBeDefined();
+        expect(button.getAttribute("disabled")).toMatch("disabled");
+      });
 
-      var itemOutStockB = document.createElement("article");
-      itemOutStockB.classList.add("out-of-stock"); 
-      list.appendChild(itemOutStockB);
-      button = document.createElement("button");
+      it('check list with two elements out of stock', function() {
+        var itemOutStockB = document.createElement("article");
+        itemOutStockB.classList.add("out-of-stock"); 
+        list.appendChild(itemOutStockB);
+        button = document.createElement("button");
 
-      //check list with two elements out of stock
-      ShopCart.apiTest.checkIfBuyButtonIsEnable(list, button);
-      expect(button.getAttribute("disabled")).toBeDefined();
-      expect(button.getAttribute("disabled")).toMatch("disabled");
+        ShopCart.apiTest.checkIfBuyButtonIsEnable(list, button);
+        expect(button.getAttribute("disabled")).toBeDefined();
+        expect(button.getAttribute("disabled")).toMatch("disabled");
+      });
     });
   });
 });
